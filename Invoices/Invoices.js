0afd4b28acc319501feaa667c280a58054bb643b
jcolameo <jeremy.colameo@gmail.com>
2022-05-07 11:28:05 +0200

    Updated

---
+++ Invoices/Invoices.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { View } from "react-native-web";
+
+const UselessTextInput = () => {
+    const [text, onChangeText] = React.useState("Insert Credit Card Information here");
+    const [number, onChangeNumber] = React.useState(null);
+
+    return (
+
+        <SafeAreaView>
+
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+            />
+
+
+
+
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeNumber}
+                value={number}
+                placeholder="useless placeholder"
+                keyboardType="numeric"
+            />
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
+
+export default UselessTextInput;
